de8f49361243d95faa1ae822abb1a138bc1e1c44
yonaMac <yonaimyo@gmail.com>
2025-05-08 06:11:30 +0900

    Feat: implement Object

---
+++ include/Object.hpp
@@ -0,0 +1,28 @@
+#ifndef OBJECT_HPP
+#define OBJECT_HPP
+
+#include <glm.hpp>
+#include "Mesh.hpp"
+#include "math.hpp"
+
+class Object
+{
+  public:
+	Mesh     *mesh;
+	glm::vec3 position;
+	glm::vec3 rotation; // Euler angles
+	glm::vec3 scale;
+
+	Object() = default;
+
+	glm::mat4 getModelMatrix() const
+	{
+		glm::mat4 t = math::translate(position);
+		glm::mat4 r = math::rotate(rotation);
+		glm::mat4 s = math::scale(scale);
+
+		return t * r * s;
+	}
+};
+
+#endif
