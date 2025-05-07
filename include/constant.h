6450650fbadbacd7877d0554f4a55ff7bc0e3247
yonaMac <yonaimyo@gmail.com>
2025-05-08 04:42:09 +0900

    Rename: constant.h

--- include/constant.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONSTANT_H
-#define CONSTANT_H
-
-
-
-// infiniy value
-#define FLT_INF std::numeric_limits<float>::infinity()
-
-#endif
--- include/constant.hpp
+++ include/constant.hpp
@@ -4,6 +4,9 @@
 // allowed error range
 #define EPSILON 1e-6f
 
+// infiniy value
+#define FLT_INF std::numeric_limits<float>::infinity()
+
 #define VIEWPORT_X	512
 #define VIEWPORT_Y	512
 
