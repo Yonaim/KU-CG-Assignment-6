7a540fb51346336fdfe70ba62bff725b5da59f68
yonaMac <yonaimyo@gmail.com>
2025-05-08 06:08:16 +0900

    Feat: implement Renderer

---
+++ include/Renderer.hpp
@@ -0,0 +1,28 @@
+#ifndef RENDERER_HPP
+#define RENDERER_HPP
+
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#else
+#include <Windows.h>
+#include <GL/glew.h>
+#include <GL/GL.h>
+#include <GL/freeglut.h>
+#endif
+
+#include "Rasterizer.hpp"
+#include "Scene.hpp"
+
+class Renderer
+{
+  private:
+  public:
+	Scene                      scene;
+	std::vector<unsigned char> output;
+
+	Renderer();
+	~Renderer();
+	void render(Rasterizer &rasterizer);
+};
+
+#endif
---
+++ src/Renderer.cpp
@@ -0,0 +1,26 @@
+#include "Renderer.hpp"
+
+Renderer::Renderer()
+{
+}
+
+Renderer::~Renderer()
+{
+}
+
+void Renderer::render(Rasterizer &rasterizer)
+{
+	rasterizer.framebuffer.clear_color(glm::vec3(0.0f));
+	rasterizer.framebuffer.clear_depth();
+
+	for (const auto &object : scene.objects)
+	{
+		rasterizer.vertex_shader->model = object.getModelMatrix();
+
+		for (int i = 0; i < object.mesh->n_triangles; i++)
+		{
+			std::array<Vertex, 3> triangle = object.mesh->get_nth_triangle(i);
+			rasterizer.draw_triangle(triangle[0], triangle[1], triangle[2]);
+		}
+	}
+}
