5cd1093cd04d6b9c867b34a571790344b0d574a9
yonaMac <yonaimyo@gmail.com>
2025-05-08 11:25:07 +0900

    Feat: implement Camera setter

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,8 +1,9 @@
 #include "Camera.hpp"
+#include "math.hpp"
 
 Camera::Camera()
 	: eye(0.0f), right(1.0f, 0.0f, 0.0f), up(0.0f, 1.0f, 0.0f),
-	  forward(0.0f, 0.0f, 1.0f), distance(1.0f)
+	  forward(0.0f, 0.0f, 1.0f)
 {
 }
 
@@ -13,17 +14,35 @@ void Camera::setEye(glm::vec3 e)
 	eye = e;
 }
 
-void Camera::setDirection(glm::vec3 u, glm::vec3 v, glm::vec3 w)
+void Camera::setDirection(glm::vec3 r, glm::vec3 u, glm::vec3 f)
 {
-	right   = glm::normalize(u); // u
-	up      = glm::normalize(v); // v
-	forward = glm::normalize(w); // w
+	right   = glm::normalize(r);
+	up      = glm::normalize(u);
+	forward = glm::normalize(f);
 }
 
-void Camera::setDistance(float d)
+glm::vec3 Camera::getEye() const
 {
-	if (d <= 0.0f)
-		std::cerr << "[Camera] Invalid distance: " << d << std::endl;
-	else
-		distance = d;
+	return eye;
+}
+
+glm::vec3 Camera::getRight() const
+{
+	return right;
+}
+
+glm::vec3 Camera::getUp() const
+{
+	return up;
+}
+
+glm::vec3 Camera::getForward() const
+{
+	return forward;
+}
+
+glm::mat4 Camera::getViewMatrix() const
+{
+	glm::vec3 lookat = eye + forward;
+	return (math::view(eye, lookat, up));
 }
