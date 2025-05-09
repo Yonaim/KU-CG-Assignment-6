b6edfe9e1ce7620033855e29e5141ab94344ba8f
yonaMac <yonaimyo@gmail.com>
2025-05-10 05:36:19 +0900

    Feat: Apply ShaderGlobalState to FragmentShader

--- include/FragmentShader.hpp
+++ include/FragmentShader.hpp
@@ -2,14 +2,22 @@
 #define FRAGMENTSHADER_HPP
 
 #include "ShaderIO.hpp"
+#include "ShaderGlobalState.hpp"
 
 // Abstract class
-// because I later add multiple fragment shaders
 class FragmentShader
 {
+  protected:
+	const ShaderGlobalState *global_state = nullptr;
+
   public:
 	virtual glm::vec3 fragment(const FragmentIn &in) const = 0;
 	virtual ~FragmentShader()                              = default;
+
+	virtual void set_global_state(const ShaderGlobalState *state)
+	{
+		global_state = state;
+	}
 };
 
 #endif
