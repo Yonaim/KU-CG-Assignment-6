05cbc84de5654d653f7496ee3a2e2de8d72f076b
yonaMac <yonaimyo@gmail.com>
2025-05-10 05:30:17 +0900

    Feat: diverted VertexShader logic by shading mode

--- include/VertexShader.hpp
+++ include/VertexShader.hpp
@@ -22,10 +22,18 @@ class VertexShader
 	VertexOut vertex(const Vertex &in) const
 	{
 		VertexOut out;
+
 		glm::vec4 world_pos = model * glm::vec4(in.position, 1.0f);
 		out.world_pos       = glm::vec3(world_pos);
 		out.position        = projection * view * world_pos;
 		out.normal = glm::mat3(glm::transpose(glm::inverse(model))) * in.normal;
+
+// if shading mode is Gouarud shading, then per vertex color is needed
+#if SHADING_MODE_GOURAUD
+		out.color = blinn_phong(glm::normalize(out.normal), out.world_pos,
+								global_state->view_pos, *global_state->material,
+								*global_state->lighting);
+#endif
 		return out;
 	}
 };
