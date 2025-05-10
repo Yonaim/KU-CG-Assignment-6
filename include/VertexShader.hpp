353207e27eb3f84009e343c444041e875398c465
yonaMac <yonaimyo@gmail.com>
2025-05-11 04:17:57 +0900

    Refactor: divide declaration and definitions

--- include/VertexShader.hpp
+++ include/VertexShader.hpp
@@ -1,9 +1,9 @@
 #ifndef VERTEXSHADER_HPP
 #define VERTEXSHADER_HPP
 
+#include <glm/glm.hpp>
 #include "ShaderIO.hpp"
 #include "ShaderGlobalState.hpp"
-#include "shading_utils.hpp"
 #include "ShadingMode.hpp"
 
 class VertexShader
@@ -15,30 +15,9 @@ class VertexShader
 
 	const ShaderGlobalState *global_state = nullptr;
 
-	void set_global_state(const ShaderGlobalState *state)
-	{
-		global_state = state;
-	}
+	void set_global_state(const ShaderGlobalState *state);
 
-	VertexOut vertex(const Vertex &in) const
-	{
-		VertexOut out;
-
-		glm::vec4 world_pos = model * glm::vec4(in.position, 1.0f);
-		out.world_pos       = glm::vec3(world_pos);
-		out.position        = projection * view * world_pos;
-		out.normal = glm::mat3(glm::transpose(glm::inverse(model))) * in.normal;
-
-		// if shading mode is Gouarud shading, then per vertex color is needed
-		if (global_state->shading_mode == ShadingMode::Gouraud)
-			out.color
-				= blinn_phong(glm::normalize(out.normal), out.world_pos,
-							  global_state->view_pos, *global_state->material,
-							  *global_state->lighting);
-		else
-			;
-		return out;
-	}
+	VertexOut vertex(const Vertex &in) const;
 };
 
 #endif
